1b65eee33fa10c7873b01cd01bf48d84ba2b815a
Lucas Cardoso <lucas.cardosopessoal@hotmail.com>
2021-08-16 20:49:24 -0300

    Investidor Map Test

--- src/br/com/fiap/investimento/test/InvestidorMapTest.java
+++ src/br/com/fiap/investimento/test/InvestidorMapTest.java
@@ -18,6 +18,7 @@ public class InvestidorMapTest {
 		List<InvestidorPF> lista = new ArrayList<>();
 		Set<InvestidorPF> setlist = new HashSet<>();
 		
+		/*
 		// Map 
 		Map<String, String> mapaNomes = new TreeMap<String, String>();
 		mapaNomes.put("chave 2", "valor 2");
@@ -27,6 +28,45 @@ public class InvestidorMapTest {
 		mapaNomes.put("chave 2", "valor 222");
 		
 		System.out.println(mapaNomes);
+		*/
+		
+		InvestidorPF i1 = new InvestidorPF(191, "Flávio", 10);
+		InvestidorPF i2 = new InvestidorPF(192, "Eduardo", 20);
+		InvestidorPF i3 = new InvestidorPF(193, "Moreni", 30);
+		InvestidorPF i4 = new InvestidorPF(194, "Silva", 40);
+		InvestidorPF i5 = new InvestidorPF(193, "Moreni 193-5", 30);
+		InvestidorPF i6 = new InvestidorPF(193, "Moreni 193-6", 30);
+		
+		Map<Integer, InvestidorPF> mapa = new HashMap<Integer, InvestidorPF>();
+		mapa.put(i1.getInvestidorId(), i1);
+		mapa.put(i2.getInvestidorId(), i2);
+		mapa.put(i3.getInvestidorId(), i3);
+		mapa.put(i4.getInvestidorId(), i4);
+		mapa.put(i5.getInvestidorId(), i5);
+		mapa.put(i6.getInvestidorId(), i6);
+		
+		Integer idBusca = 194;
+		System.out.println(mapa.containsKey(idBusca));
+		System.out.println(mapa.containsValue(null));
+		
+		InvestidorPF invBusca = mapa.get(idBusca);
+		System.out.println(invBusca);
+		
+		
+		//mapa.keySet().stream().filter(m -> "Flavio".equals(m.getInteger(1))); 
+		
+		for (Integer id : mapa.keySet()) {
+			System.out.println(id);
+		}
+		
+	
+		
+		
+		for (InvestidorPF investidorPF : mapa.values()) {
+			System.out.println(investidorPF);
+		}
+		
+		//System.out.println(mapa);
 		
 	}
 
